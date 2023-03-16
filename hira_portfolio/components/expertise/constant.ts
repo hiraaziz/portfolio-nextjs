6695c5e2cf039516d4c9e71188601763ef0016db
hiraaziz <hiraaziz1998@gmail.com>
2023-03-17 02:57:41 +0500

    Create constant.ts

---
+++ hira_portfolio/components/expertise/constant.ts
@@ -0,0 +1,46 @@
+export type expertiseType = {
+  title: string;
+  percent: number;
+};
+export const frontend: expertiseType[] = [
+  {
+    title: "React.js",
+    percent: 70,
+  },
+  {
+    title: "Typescript",
+    percent: 60,
+  },
+  {
+    title: "Javascript",
+    percent: 80,
+  },
+];
+export const design: expertiseType[] = [
+  {
+    title: "Figma",
+    percent: 70,
+  },
+  {
+    title: "ChakraUI",
+    percent: 80,
+  },
+  {
+    title: "Tailwind css",
+    percent: 80,
+  },
+];
+export const backend: expertiseType[] = [
+  {
+    title: "Node.js",
+    percent: 60,
+  },
+  {
+    title: "Mongo DB",
+    percent: 70,
+  },
+  {
+    title: "AWS",
+    percent: 30,
+  },
+];
