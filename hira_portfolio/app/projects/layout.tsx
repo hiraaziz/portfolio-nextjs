e68cf751967c6ef4bee5b4d46a0a889d3b8e7b63
hiraaziz <hiraaziz1998@gmail.com>
2023-03-17 18:12:21 +0500

    Update layout.tsx

--- hira_portfolio/app/projects/layout.tsx
+++ hira_portfolio/app/projects/layout.tsx
@@ -5,17 +5,8 @@ import Image from "next/image";
 import React, { useRef } from "react";
 
 const layout = ({ children }: { children: React.ReactNode }) => {
-  const expertiseRef = useRef<HTMLDivElement>(null);
-  const contactRef = useRef<HTMLDivElement>(null);
-  const servicesRef = useRef<HTMLDivElement>(null);
   return (
     <>
-      <Nav
-        background={"tranparent"}
-        scrolexp={expertiseRef}
-        scrolcontact={contactRef}
-        scrolservice={servicesRef}
-      />
       <div className="bg-secondary">
         <div className="relative w-full h-[200px] md:h-[400px] overflow-hidden">
           <Image
