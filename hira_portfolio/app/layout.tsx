a825da9dc4a9e0293f4a722448d2f9e35e6633c2
hiraaziz <hiraaziz1998@gmail.com>
2023-03-17 19:27:03 +0500

    Update layout.tsx

--- hira_portfolio/app/layout.tsx
+++ hira_portfolio/app/layout.tsx
@@ -1,6 +1,7 @@
 import ThemeSwitcher from "@/components/ThemeSwitcher";
 import "./globals.css";
 import Footer from "@/components/Footer";
+import Contextapi from "@/components/Contextapi";
 export default function RootLayout({
   children,
 }: {
@@ -11,7 +12,7 @@ export default function RootLayout({
       <head />
       <body className="font-poppins">
         <ThemeSwitcher>
-          {children}
+          <Contextapi>{children}</Contextapi>
           <Footer />
         </ThemeSwitcher>
       </body>
