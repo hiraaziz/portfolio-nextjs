48e896ce9c8881cc7a752b196caa61c23480ce52
hiraaziz <hiraaziz1998@gmail.com>
2023-03-19 13:27:47 +0500

    Update Navwrapper.tsx

--- hira_portfolio/components/Navwrapper.tsx
+++ hira_portfolio/components/Navwrapper.tsx
@@ -3,15 +3,27 @@ import React, { useRef, useContext } from "react";
 import Nav from "./Nav";
 import { Navcontext } from "./Contextapi";
 
+type scrolType = {
+  title: string;
+  link: string;
+};
+
 export const Navwrapper = ({ background }: { background: string }) => {
   const { expertiseRef, contactRef, servicesRef } = useContext(Navcontext);
+  const scrollinks: scrolType[] = [
+    {
+      title: "EXPERTISE",
+      link: "/",
+    },
 
-  return (
-    <Nav
-      background={background}
-      scrolexp={expertiseRef}
-      scrolcontact={contactRef}
-      scrolservice={servicesRef}
-    />
-  );
+    {
+      title: "SERVICES",
+      link: "/",
+    },
+    {
+      title: "CONTACT",
+      link: "/",
+    },
+  ];
+  return <Nav background={background} scrollink={scrollinks} />;
 };
