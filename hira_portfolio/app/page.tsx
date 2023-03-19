ce53d48693c6eb505393e59a5e653f3761cd14a1
hiraaziz <hiraaziz1998@gmail.com>
2023-03-19 13:27:42 +0500

    Update page.tsx

--- hira_portfolio/app/page.tsx
+++ hira_portfolio/app/page.tsx
@@ -1,25 +1,37 @@
 "use client";
-import React, { useRef, useContext } from "react";
+import React, { useContext } from "react";
 import Banner from "@/components/Banner";
 import Expertise from "@/components/expertise/Expertise";
 import Portfolio from "@/components/portfolio/Portfolio";
 import Contact from "@/components/Contact";
 import Nav from "@/components/Nav";
 import Services from "@/components/Services";
-import { Navwrapper } from "@/components/Navwrapper";
 import Contextapi, { Navcontext } from "@/components/Contextapi";
 
+type scrolType = {
+  title: string;
+  link: React.RefObject<HTMLDivElement>;
+};
 export default function Home() {
   const { expertiseRef, contactRef, servicesRef } = useContext(Navcontext);
+  const scrollinks: scrolType[] = [
+    {
+      title: "EXPERTISE",
+      link: expertiseRef,
+    },
 
+    {
+      title: "SERVICES",
+      link: contactRef,
+    },
+    {
+      title: "CONTACT",
+      link: servicesRef,
+    },
+  ];
   return (
     <div className=" bg-secondary">
-      <Nav
-        background={"primarylight"}
-        scrolexp={expertiseRef}
-        scrolcontact={contactRef}
-        scrolservice={servicesRef}
-      />
+      <Nav background={"primarylight"} scrollinks={scrollinks} />
 
       <Banner />
       <Expertise scrol={expertiseRef} />
